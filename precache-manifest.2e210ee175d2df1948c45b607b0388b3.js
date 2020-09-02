dc81a69b33eb2718713d419b6ef34ff64e7fc20c
Eugenio Pilastro Luca <507904+E-PL@users.noreply.github.com>
2020-09-02 19:01:54 +0200

    Updates

---
+++ precache-manifest.2e210ee175d2df1948c45b607b0388b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7869f61598642532d6010ebbf6d1c5d0",
+    "url": "/would-you-rather/index.html"
+  },
+  {
+    "revision": "1218c52781fe86064a59",
+    "url": "/would-you-rather/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "e3a6909c33554ea6f4f8",
+    "url": "/would-you-rather/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1218c52781fe86064a59",
+    "url": "/would-you-rather/static/js/2.23b02150.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/would-you-rather/static/js/2.23b02150.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3a6909c33554ea6f4f8",
+    "url": "/would-you-rather/static/js/main.9f9a9247.chunk.js"
+  },
+  {
+    "revision": "d626189d7e50a856e143",
+    "url": "/would-you-rather/static/js/runtime-main.c914aebb.js"
+  }
+]);
